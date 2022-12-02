26b49cdbdef9f2bc524f0d16af8f95641bfd269c
Oyat0824 <langue0824@gmail.com>
2022-12-02 20:20:16 +0900

    ArticleController에 테스트 데이터 및 추가, 리스트, 삭제, 수정 구현

--- src/main/java/com/kij/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kij/exam/demo/controller/UsrHomeController.java
@@ -1,14 +1,43 @@
 package com.kij.exam.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.kij.exam.demo.vo.Article;
+
 @Controller
 public class UsrHomeController {
-	@RequestMapping("/usr/home/main")
+	@RequestMapping("/usr/home/getMap")
 	@ResponseBody
-	public String showMain() {
-		return "안녕하세요.";
-	}	
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수나이", 22);
+		map.put("영희나이", 24);
+		
+		return map;
+	}
+	
+	@RequestMapping("/usr/home/getList")
+	@ResponseBody
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("철수나이");
+		list.add("철수나이2");
+		
+		return list;
+	}
+	
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article();
+		
+		return article;
+	}
 }
